2ae8a9dc87f9ab158a79e5b0a524324820ab8db1
Kyo young Lim <limkyouyou@gmail.com>
2023-03-27 09:42:33 +0900

    Add favicon

--- yougetsite/yougetsite/urls.py
+++ yougetsite/yougetsite/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf import settings
+import os
+
+app_name = 'yougetsite'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
